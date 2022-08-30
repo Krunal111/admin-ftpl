816d26c2922ce101dae1fc275c140e7c8853b5bd
Abddeali Kachwala <abdeali@vcaretechnologies.net>
2022-08-30 21:02:31 +0530

    FTPL Admin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,129 +1,11 @@
-// Header Fixed JS
-jQuery(document).ready(function()
-{
-    jQuery(window).scroll(function() {    
-        var scroll = jQuery(window).scrollTop();
-        if (scroll >= 100) {
-            jQuery(".header").addClass("fixed-header");
-        } else {
-            jQuery(".header").removeClass("fixed-header");
-        }
+$(document).ready(function() {
+    $(".sidebar-header").click(function(){
+        $(".sidebar, .main-content").toggleClass("collapsed");
+        $(this).toggleClass("show");
+    })
+    $(".mobile__toggle").click(function(){
+      $(".header__menu").slideToggle();
     });
-});
-// Header Fixed JS
-
-// Hero JS Start
-var heroSlider = new Swiper(".hero__banner__slider", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-      delay: 6000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      0: {
-        autoHeight: true
-      },
-      1024: {
-        slidesPerView: 1
-      }
-    }
-});
-// Hero JS End
-autoHeight: true
-// Popular Products JS Start
-var popularProductsSlider = new Swiper(".popular__products__slider", {
-    slidesPerView: 1,
-    spaceBetween: 24,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-        0: {
-          slidesPerView: 2,
-          spaceBetween: 8
-        },
-        768: {
-          slidesPerView: 3
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 24
-        }
-    }
-});
-// Popular Products JS End
-
-// Diamond Products JS Start
-var diamondProductsSlider = new Swiper(".diamond__products__slider", {
-    slidesPerView: 1,
-    spaceBetween: 24,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 2,
-        spaceBetween: 8
-      },
-      768: {
-        slidesPerView: 3
-      },
-      1024: {
-        slidesPerView: 5,
-        spaceBetween: 24
-      }
-    }
-});
-// Diamond Products JS End
-
-// Rings Slider JS Start
-var ringsSlider = new Swiper(".rings__slider", {
-    slidesPerView: 3,
-    grid: {
-      rows: 2,
-    },
-    // autoplay: {
-    //   delay: 4000,
-    //   disableOnInteraction: false,
-    // },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 2
-      },
-      1024: {
-        slidesPerView: 3
-      }
-    }
-});
-// Rings Slider JS End
-
-var productThumb = new Swiper(".product__thumb__image", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var productLarge = new Swiper(".product__large__image", {
-  thumbs: {
-    swiper: productThumb,
-  },
 });
 
 // SVG file to SVG code convert JS Start
@@ -153,14 +35,3 @@ function img2svg() {
 }
 img2svg();
 // SVG file to SVG code convert JS End
-
-// Filter By JS
-jQuery(document).ready(function()
-{
-    $('.category__filter__header').click(function () {
-        if ($(window).width() < 767) {
-            $('.product__category__list').toggleClass("show");
-        }
-    });
-});
-// Filter By JS
